cba3006f6571c1d5c8abf426ad4c3af3fc374fbf
Rowan Jansens <rowan.jansens@gmail.com>
2023-02-28 15:19:13 -0500

    added base funcitons

---
+++ nozzle.m
@@ -0,0 +1,3 @@
+function [h_out, s_out, T_out, P_out, W_dot] = nozzle(h_in, s_in, T_in, P_in
+
+end
---
+++ turbine.m
@@ -0,0 +1,3 @@
+function [h_out, s_out, T_out, P_out, W_dot] = turbine(h_in, s_in, T_in, P_in
+
+end
