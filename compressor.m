ae493f85d38bfae23eb5d8d74050daba205d5820
Rowan Jansens <rowan.jansens@gmail.com>
2023-03-01 00:19:58 -0500

    Update compressor.m

--- compressor.m
+++ compressor.m
@@ -1,4 +1,18 @@
-function [h_out, s_out, T_out, P_out, W_dot] = compressor(h_in, s_in, T_in, P_in)
+function [h_out, s_out, T_out, P_out, W_dot] = compressor(h_in, s_in, T_in, P_in, pressure_ratio)
+    % calculate input state using inital conditions
+    s_in = refpropm('S', 'T', T_in, 'P', P_in, 'air');
+    h_in = refpropm('H', 'T', T_in, 'P', P_in, 'air');
+
+    %assume isentropic compressor
+    s_out = s_in;
+
+    %calculate presssure from pressure ratio
+    P_2 = P_1 * pressure_ratio;
+
+    %other parameters using refprop
     
+
+
+
 
 end
