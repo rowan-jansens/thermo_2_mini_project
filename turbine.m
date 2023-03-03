0e52d8d2ba238d16b6a0b9a1464e73533174eafd
Ethan C <36670680+etbikehome@users.noreply.github.com>
2023-03-03 15:28:12 -0500

    Update turbine.m

--- turbine.m
+++ turbine.m
@@ -1,9 +1,10 @@
 function [h_out, s_out, T_out, P_out, w_dot] = turbine(h_in, s_in, T_in, P_in)
+global fluid
 P_out = 1000;
-s_out = refpropm('S', 'T', T_in, 'P', P_in, 'air');
+s_out = refpropm('S', 'T', T_in, 'P', P_in, fluid);
 s_out_check = s_out-s_in
-h_out = refpropm('H', 'P', P_out, 'S', s_out, 'air');
-T_out = refpropm('T', 'P', P_out, 'S', s_out, 'air');
+h_out = refpropm('H', 'P', P_out, 'S', s_out, fluid);
+T_out = refpropm('T', 'P', P_out, 'S', s_out, fluid);
 w_dot = (h_in-h_out);
 
 end
