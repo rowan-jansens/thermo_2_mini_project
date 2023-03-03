9079a830462fbe45a7a4ef4e451eab7f2e7c962c
william-skelly <wskelly@olin.edu>
2023-03-03 13:49:21 -0500

    Added Nozzle Function

--- nozzle.m
+++ nozzle.m
@@ -1,3 +1,18 @@
-function [h_out, s_out, T_out, P_out, W_dot] = nozzle(h_in, s_in, T_in, P_in
+%%%
+% Calculates the thrust and Sgen of jet nozzle
+%
+% Args:
+%   h4: enthalpy at state 4 (into nozzle) [J/kg]
+%   h1: enthalpy at state 1 (out of nozzle) [J/kg]
+%   v_in: velocity of plane [m/s]
+%   m_dot: mass flow rate [kg/s]
+% Returns:
+%   thrust: thrust of engine [N]
+%%%
 
+function thrust = nozzle(h4, h1, v_in, m_dot)
+    % Assume adiabatic nozzle
+    h_delta = h4 - h1
+    v_out = sqrt(v_in^2+2*h_delta)
+    thrust = m_dot * (v_out - v_in)
 end
