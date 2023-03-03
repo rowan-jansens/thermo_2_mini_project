baf3f1f98e7457b7914e236467d2a49b62528226
Rowan Jansens <rowan.jansens@gmail.com>
2023-03-03 16:39:20 -0500

    add second law efficiency to compressor

--- compressor.m
+++ compressor.m
@@ -1,5 +1,5 @@
-function [h_out, s_out, T_out, P_out, W_dot] = compressor(h_in, s_in, T_in, P_in, pressure_ratio)
-   global fluid
+function [h_out, s_out, T_out, P_out, W_dot, S_gen] = compressor(h_in, s_in, T_in, P_in, pressure_ratio, n_compressor)
+   global fluid mass_flow_rate
 
     %assume isentropic compressor
     s_out = s_in;
@@ -9,9 +9,15 @@ function [h_out, s_out, T_out, P_out, W_dot] = compressor(h_in, s_in, T_in, P_in
 
     %other parameters using refprop
     h_out = refpropm('H', 'P', P_out, 'S', s_out, fluid);
-    T_out = refpropm('T', 'P', P_out, 'S', s_out, fluid);
-
+    
     %calculate work
-    W_dot = (h_out - h_in);
+    W_dot_ideal = mass_flow_rate*(h_out - h_in);
+    W_dot = W_dot_ideal/n_compressor;
+
+    h_out = (W_dot/mass_flow_rate) + h_in;
+    T_out = refpropm('T', 'P', P_out, 'h', h_out, fluid);
+    s_out = refpropm('S', 'P', P_out, 'h', h_out, fluid);
+
+    S_gen = mass_flow_rate*(s_out-s_in)
 
 end
