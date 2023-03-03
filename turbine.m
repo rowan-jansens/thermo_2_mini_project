380d70e32fbe2ba40873200852acd320f6e3194c
Ethan C <36670680+etbikehome@users.noreply.github.com>
2023-03-03 16:32:53 -0500

    update turbine

--- turbine.m
+++ turbine.m
@@ -1,14 +1,18 @@
-function [h_out, s_out, T_out, P_out] = turbine(h_in, s_in, T_in, P_in, w_dot_compressor)
+function [h_out_real, s_out, T_out, P_out] = turbine(h_in, s_in, T_in, P_in, w_dot_compressor, n_turbine)
 global fluid
 
-
-s_out = s_in;
-
-h_out = h_in-w_dot_compressor ;
+s_out_ideal = s_in;
+h_out_ideal = h_in-w_dot_compressor;
 
 
-T_out = refpropm('T', 'H', h_out, 'S', s_out, fluid);
-P_out = refpropm('P', 'H', h_out, 'S', s_out, fluid);
+
+
+w_dot_real = w_dot_compressor*effeciency;
+h_out_real = h_in -w_dot_real;
+
+s_out = refpropm('S', 'P', , 'H', h_out_real, fluid);
+T_out = refpropm('T', 'H', h_out_real, 'S', s_out, fluid);
+P_out = refpropm('P', 'H', h_out_real, 'S', s_out, fluid);
 
 
 end
