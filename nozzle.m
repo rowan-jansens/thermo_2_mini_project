cc191a13759f13126318091e49641de81f8c519d
william-skelly <wskelly@olin.edu>
2023-03-03 16:01:26 -0500

    Now accounts for heat loss to environment

--- nozzle.m
+++ nozzle.m
@@ -5,13 +5,15 @@
 %   h4: enthalpy at state 4 (into nozzle) [J/kg]
 %   h1: enthalpy at state 1 (out of nozzle) [J/kg]
 %   v_plane: velocity of plane [m/s]
+%   q_spef: spefic heat transfer rate from exhaust steam to
+%       environment [W/(kg/s)]
 % Returns:
 %   thrust_spef: specific thrust of engine [N/(kg/s)]
 %%%
 
-function thrust_spef = nozzle(h4, h1, v_plane)
-    % Assume adiabatic nozzle
+function thrust_spef = nozzle(h4, h1, v_plane, q_spef)
+    % No longer assumes Assume adiabatic nozzle
     h_delta = h4 - h1;
-    v_out = sqrt(v_plane^2+2*h_delta);
+    v_out = sqrt(v_plane^2+2*h_delta*q_spef);
     thrust_spef = (v_out - v_plane);
 end
