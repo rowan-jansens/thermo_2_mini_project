603b3be1b662d4bee41d12f5fd752d8442db6d56
DBurns451 <91289717+DBurns451@users.noreply.github.com>
2023-03-05 22:19:36 -0500

    Implemented density

--- combustor.m
+++ combustor.m
@@ -1,4 +1,4 @@
-function [h_out, s_out, T_out, P_out, Q_dot_comb,fuel_m_dot] = combustor(h_in, s_in, T_in, P_in, T_max)
+function [h_out, s_out, T_out, P_out, Q_dot_comb,fuel_m_dot,rho_out_comb] = combustor(h_in, s_in, T_in, P_in, T_max)
 
 global fluid mass_flow_rate
 
@@ -6,8 +6,10 @@ P_out = P_in;                                    % Combustion chamber is isobari
 T_out = T_max;                                   % Outputs the maximum combustion chamber pressure (design parameter)
 
 
-s_out = refpropm('S','T',T_out,'P',P_out,fluid); % Calculates the entropy at combustion chamber output
-h_out = refpropm('H','T',T_out,'P',P_out,fluid); % Calculates the enthalpy at combustion chamber output
+s_out = refpropm('S','T',T_out,'P',P_out,fluid); % Calculates the entropy at combustion chamber outlet
+h_out = refpropm('H','T',T_out,'P',P_out,fluid); % Calculates the enthalpy at combustion chamber outlet
+
+rho_out_comb = refpropm('D','T',T_out,'P',P_out,fluid); % Calculates density of air at combustion chamber outlet
 
 Q_dot_comb = (h_out - h_in) * mass_flow_rate;
 
